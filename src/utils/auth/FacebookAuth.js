109c7ad85efc6ebe2569f1e28a98330b7acd3427
unknown <nachappapr1991@gmail.com>
2021-05-13 08:30:58 +0530

    changes to fb auth

--- src/utils/auth/FacebookAuth.js
+++ src/utils/auth/FacebookAuth.js
@@ -1,68 +1,14 @@
-import { useEffect } from "react";
 // import { Button } from "../../components";
 
 const FacebookAuth = () => {
-  //   const [log, setLog] = useState(null);
-  useEffect(() => {
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: "test",
-        cookie: true,
-        xfbml: true,
-        version: "v10.0",
-      });
-
-      window.FB.AppEvents.logPageView();
-    };
-
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  }, []);
-
-  window.FB.getLoginStatus(function (response) {
-    statusChangeCallback(response);
-  });
-
-  const statusChangeCallback = (response) => {
-    if (response.status === "connected") {
-      var accessToken = response.authResponse.accessToken;
-      console.log(accessToken);
-      //   setLog(true);
-    }
-  };
-
-  //   const checkLoginState = () => {
-  //     window.FB.getLoginStatus(function (response) {
-  //       statusChangeCallback(response);
-  //     });
-  //   };
-
-  const handleClick = () => {
-    // if (log) {
-    //   window.FB.logout(function (response) {
-    //     console.log("loggedOut");
-    //     window.location("/");
-    //   });
-    // } else {
-    window.FB.login();
-    // }
-    // console.log(log);
-    // console.log(status);
-  };
-
   return (
     <div>
-      {/* {console.log(window.FB)} */}
-      <button className="buttonfacebook" onClick={handleClick}>
+      <button
+        className="buttonfacebook"
+        onClick={() => {
+          window.FB.logout();
+        }}
+      >
         Login with facebook
       </button>
     </div>
