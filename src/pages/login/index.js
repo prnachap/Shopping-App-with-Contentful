b910338b92f079e717d0c1fe18834961f1960f3a
unknown <nachappapr1991@gmail.com>
2021-05-13 08:48:04 +0530

    removed FB auth

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import classes from "./index.module.scss";
 import { SignUp } from "../../components";
-import FacebookAuth from "../../utils/auth/FacebookAuth";
 
 const Login = () => {
   return (
     <div className={classes.login}>
       <SignUp />
-      <FacebookAuth />
     </div>
   );
 };
